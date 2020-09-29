a08855227c60856c0a05837239b6121a6c321ce6
wfc <fwu9@stevens.edu>
2020-09-28 21:17:57 -0400

    add tangent function

--- HW1.py
+++ HW1.py
@@ -4,7 +4,15 @@ import matplotlib.pyplot as plt
 x = np.arange(0,2 * np.pi,0.01)
 ysin = np.sin(x)
 ycos = np.cos(x)
+ytan = np.tan(x)
+
+
 
 plt.plot(x, ysin,color='black', linewidth=1, alpha=0.6, label="sin(x)")
 plt.plot(x, ycos,color='red', linewidth=1, alpha=0.6, label='cos(x)')
+##------ changed by Fangchi Wu ---------##
+plt.plot(x, ytan, color = 'yellow', linewidth = 1,alpha=0.6, label='tan(x)' )
+plt.ylim(ymax = 2.5)
+plt.ylim(ymin = -2.5)
+## ----------------------------------------- ##
 plt.show()
